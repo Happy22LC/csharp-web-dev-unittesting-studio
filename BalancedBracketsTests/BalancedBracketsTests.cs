c4429c2fa2689aa8c833209bb47044a8881d0038
Happy <biswasnedhi09@gmail.com>
2022-08-31 19:50:33 -0500

    studio completed

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -13,5 +13,83 @@ namespace BalancedBracketsTests
         {
             Assert.AreEqual(true, true);
         }
+
+        [TestMethod]
+        public void OnlyBracketReturnTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]") );
+        }
+        [TestMethod]
+        public void EmptyStringReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets(""));
+        }
+
+        [TestMethod]
+        public void UnbalacedBracketReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]["));
+        }
+        [TestMethod]  
+        public void OneOpenBracketReturnFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+        }
+        
+        [TestMethod]
+        public void OneClosedBracketReturnFalse() {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]"));
+        }
+
+        [TestMethod]
+        public void MisNestedBracketsReturnsFalse() {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[[]]]"));
+        }
+
+        [TestMethod] 
+        public void StringInsideBracketReturnTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[LaunchCode]"));
+        }
+
+        [TestMethod]
+        public void StringWithinBracketWithinString() {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("Launch[Code]"));
+        }
+        
+        [TestMethod]
+        public void BracketOutsideOfStringReturnsTrue() {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]LaunchCode"));
+        }
+
+        [TestMethod]
+        public void StringWithOpenBracketOnlyReturnsFalse() {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("[LaunchCode"));
+        }
+        
+        [TestMethod]
+        public void StringWithUnbalancedBracket()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("Launch]Code["));
+        }
+        /**
+         * The function BalancedBrackets should return true if and only if
+         * the input string has a set of "balanced" brackets.
+         *
+         * That is, whether it consists entirely of pairs of opening/closing
+         * brackets (in that order), none of which mis-nest. We consider a bracket
+         * to be square-brackets: [ or ].
+         *
+         * The string may contain non-bracket characters as well.
+         *
+         * These strings have balanced brackets:
+         *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+         *
+         * While these do not:
+         *   "[LaunchCode", "Launch]Code[", "[", "]["
+         *S
+         * parameter str - to be validated
+         * returns true if balanced, false otherwise
+        */
     }
 }
